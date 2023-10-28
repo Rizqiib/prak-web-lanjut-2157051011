f51e9e6125810aae98b0d7918c9572d3a0de3db4
Rizqiib <rizqi.anvbis@gmail.com>
2023-10-28 21:23:07 +0700

    commit form-controller

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,8 +1,15 @@
 <?php
 
 use CodeIgniter\Router\RouteCollection;
+use App\Controllers\UserController;
+use App\Controllers\Home;
 
 /**
  * @var RouteCollection $routes
  */
+
 $routes->get('/', 'Home::index');
+$routes->get('/profile/(:any)/(:any)', [Home::class, 'profile']);
+$routes->get('/user/profile', [UserController::class, 'profile']);
+$routes->get('/user/create', [UserController::class, 'create']);
+$routes->post('/user/store', [UserController::class, 'store']);
